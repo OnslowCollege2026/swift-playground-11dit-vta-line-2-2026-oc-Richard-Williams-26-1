9ff08dc38c0cacd3b2ea921ced5ae1393bcc7d1d
oc-Richard-Williams-26 <richard.williams@student.onslow.school.nz>
2026-05-10 20:56:17 +0000

    May 10, 2026, 8:56 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,21 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let x = 15
+        let y = 2
+
+        let added = x + y
+        let subtracted = x - y
+        let multiplied = x * y
+        let divided =  x / y
+        let remainder = x % y
+
+        let added2 = y + x
+        let subtracted2 = y - x
+        let multiplied2 = y * x
+        let divided2 =  y / x
+        let remainder2 = y % x
+
+        print("\(y)")
     }
 }
